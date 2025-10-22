e1f3686ec172869cc3fd142bbeba8aa83d65ac5f
balddrAdmin <boliklevik@gmail.com>
2025-10-23 00:40:54 +0200

    412

--- venv/23-10-25_26-10-25.py
+++ venv/23-10-25_26-10-25.py
@@ -1,6 +1,6 @@
 # 23.10.2025
 # 1342 easy
-class Solution:
+class Solution1342:
     def numberOfSteps(self, n: int) -> int:
         count = 0
         while n > 0:
@@ -10,5 +10,22 @@ class Solution:
                 n -= 1
             count += 1
         return count
+
+
+# 412 easy
+class Solution412:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+        for i in range(1, n+1):
+            if int(i)%3 == 0 and int(i)%5 == 0:
+                output.append('FizzBuzz')
+                continue
+            elif int(i)%3 == 0:
+                output.append('Fizz')
+            elif int(i)%5 == 0:
+                output.append('Buzz')
+            else:
+                output.append(str(i))
+        return output
 
 # 876 easy
