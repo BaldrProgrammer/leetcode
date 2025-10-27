a4416f19d51b1a10d1affb40b9b2855a7ade35d3
balddrAdmin <boliklevik@gmail.com>
2025-10-27 20:45:41 +0100

    66

--- 27-10-25_02-11-25.py
+++ 27-10-25_02-11-25.py
@@ -9,4 +9,13 @@ class Solution136:
                 return i
 
 
-
+# 66 easy
+class Solution66:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in digits:
+            digits[digits.index(int(i))] = str(i)
+        number = int(''.join(digits)) + 1
+        digits = []
+        for i in str(number):
+            digits.append(int(i))
+        return digits
