263519eeee9c9329944b6efc7dcc84fca3c1835f
balddrAdmin <boliklevik@gmail.com>
2025-10-28 23:57:31 +0100

    3354

--- 27-10-25_02-11-25.py
+++ 27-10-25_02-11-25.py
@@ -57,46 +57,15 @@ class Solution7:
 
 # 28.10.2025
 # 3354 easy
-class Solution:
-    def countValidSelections(self, nums: List[int]) -> int:
-        corrects = 0
-        variants = []
-        for index, i in enumerate(nums):
-            if i == 0:
-                variants.append((index, 'right'))
-                variants.append((index, 'left'))
-        for variant in variants:
-            curr = variant[0]
-            moving = variant[1]
-            numes = nums.copy()
-            while True:
-                print(numes)
-                print(variant)
-                print(moving)
-                print(curr)
-                print('')
-                if sum(numes) == 0:
-                    corrects += 1
-                    break
-                if numes[curr] == 0:
-                    if moving == 'right':
-                        curr += 1
-                    elif moving == 'left':
-                        curr -= 1
-                else:
-                    if moving == 'right':
-                        curr += 1
-                        moving = 'left'
-                    elif moving == 'left':
-                        moving = 'right'
-                        curr -= 1
-                if curr in range(0, len(numes)):
-                    if numes[curr] > 0:
-                        numes[curr] -= 1
-
-                else:
-                    print(curr)
-                    print(curr in range(0, len(numes) - 1))
-                    print(range(0, len(numes) - 1))
-                    break
-        return corrects
+class Solution3354(object):
+    def countValidSelections(self, nums):
+        total, ls, cases = sum(nums), 0, 0
+        for i in range(len(nums)):
+            rs = total - ls - nums[i]
+            if nums[i] == 0:
+                if ls == rs:
+                    cases += 2
+                elif ls == rs - 1 or ls - 1 == rs:
+                    cases += 1
+            ls += nums[i]
+        return cases
