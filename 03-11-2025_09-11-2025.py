a93a1cc7e71d18713385fef2767212b0cd28f99b
balddrAdmin <boliklevik@gmail.com>
2025-11-04 23:26:10 +0100

    171 try

--- 03-11-2025_09-11-2025.py
+++ 03-11-2025_09-11-2025.py
@@ -19,3 +19,42 @@ class Solution169:
         for i in set(nums):
             if nums.count(i) > len(nums)/2:
                 return i
+
+
+# 171 easy
+class Solution:
+    def titleToNumber171(self, col: str) -> int:
+        letters = {
+            'A': 1,
+            'B': 2,
+            'C': 3,
+            'D': 4,
+            'E': 5,
+            'F': 6,
+            'G': 7,
+            'H': 8,
+            'I': 9,
+            'J': 10,
+            'K': 11,
+            'L': 12,
+            'M': 13,
+            'N': 14,
+            'O': 15,
+            'P': 16,
+            'Q': 17,
+            'R': 18,
+            'S': 19,
+            'T': 20,
+            'U': 21,
+            'V': 22,
+            'W': 23,
+            'X': 24,
+            'Y': 25,
+            'Z': 26
+        }
+        output = 0
+        for i in range(len(col) - 1):
+            output += 26 * letters[col[i]] * (i+1)
+        output += letters[col[-1]]
+        return output
+
