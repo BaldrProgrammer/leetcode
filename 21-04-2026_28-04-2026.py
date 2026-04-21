3832a08bc6944554d091c7e0db9d73f1bbeb4856
balddrAdmin <boliklevik@gmail.com>
2026-04-21 21:39:09 +0200

    35

--- 21-04-2026_28-04-2026.py
+++ 21-04-2026_28-04-2026.py
@@ -3,9 +3,18 @@ from typing import List
 
 # 21.04.2026
 # 27 easy
-class Solution:
+class Solution27:
     def removeElement(self, nums: List[int], val: int) -> int:
         while val in nums:
             nums.remove(val)
 
         return len(nums)
+
+
+# 21.04.2026
+# 35 easy
+class Solution35:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        set(nums)
+        nums.append(target)
+        return sorted(nums).index(target)
