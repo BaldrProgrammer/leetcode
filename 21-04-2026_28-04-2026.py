8857f84da8092748d6928f0c7961ad45e11a0786
balddrAdmin <boliklevik@gmail.com>
2026-04-22 01:05:04 +0200

    200

--- 21-04-2026_28-04-2026.py
+++ 21-04-2026_28-04-2026.py
@@ -28,28 +28,32 @@ class Solution58:
 
 
 # 21.04.2026
-# 200 medium
+# 200 medium(hard)
 class Solution200:
     def numIslands(self, grid: List[List[str]]) -> int:
-        number_of_islands = 0
-        for spisok_index, spisok in enumerate(grid):
-            is_continue = False
-            for yacheika_index, yacheika in enumerate(spisok):
-                if yacheika == '1':
-                    if spisok_index+1 < len(grid) and yacheika_index+1 < len(spisok):
-                        if grid[spisok_index+1][yacheika_index] == '0':
-                            if spisok[yacheika_index+1] == '0' and not is_continue:
-                                number_of_islands += 1
-                        else:
-                            is_continue = True
-                    else:
-                        if yacheika_index+1 < len(spisok): ## проверка крайних
-                            if spisok[yacheika_index+1] == '0' and not is_continue:
-                                number_of_islands += 1
-                        elif spisok_index+1 < len(grid): ## проверка нижних
-                            if grid[spisok_index+1][yacheika_index] == '0' and not is_continue:
-                                number_of_islands += 1
-                        else:
-                            number_of_islands += 1
+        if not grid:
+            return 0
 
-        return number_of_islands
+        rows, cols = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return
+            if grid[r][c] == '0':
+                return
+
+            grid[r][c] = '0'
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    islands += 1
+                    dfs(r, c)
+
+        return islands
