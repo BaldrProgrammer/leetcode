99cd30dc3f1db4b4da47a0ba9ea8c74c844ab232
balddrAdmin <boliklevik@gmail.com>
2026-04-22 00:03:40 +0200

    200 4

--- 21-04-2026_28-04-2026.py
+++ 21-04-2026_28-04-2026.py
@@ -33,26 +33,23 @@ class Solution200:
     def numIslands(self, grid: List[List[str]]) -> int:
         number_of_islands = 0
         for spisok_index, spisok in enumerate(grid):
+            is_continue = False
             for yacheika_index, yacheika in enumerate(spisok):
                 if yacheika == '1':
-                    if spisok_index+1 >= len(grid) and yacheika_index+1 >= len(spisok):
-                        number_of_islands += 1
-                        continue
-                    elif spisok_index+1 >= len(grid):
-                        if spisok[yacheika_index + 1] == '0':
-                            number_of_islands += 1
-                            continue
-                    elif yacheika_index+1 >= len(spisok):
-                        if grid[spisok_index + 1][yacheika_index] == '0':
-                            number_of_islands += 1
-                            continue
+                    if spisok_index+1 < len(grid) and yacheika_index+1 < len(spisok):
+                        if grid[spisok_index+1][yacheika_index] == '0':
+                            if spisok[yacheika_index+1] == '0' and not is_continue:
+                                number_of_islands += 1
+                        else:
+                            is_continue = True
                     else:
-                        if spisok[yacheika_index+1] == '0' and grid[spisok_index+1][yacheika_index] == '0':
+                        if yacheika_index+1 < len(spisok): ## проверка крайних
+                            if spisok[yacheika_index+1] == '0' and not is_continue:
+                                number_of_islands += 1
+                        elif spisok_index+1 < len(grid): ## проверка нижних
+                            if grid[spisok_index+1][yacheika_index] == '0' and not is_continue:
+                                number_of_islands += 1
+                        else:
                             number_of_islands += 1
-                            continue
 
         return number_of_islands
-
-
-solution = Solution200()
-print(solution.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
