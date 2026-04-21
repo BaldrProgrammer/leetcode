90f9e01d4d8607dae27fdf2e04fb45bc1697e144
balddrAdmin <boliklevik@gmail.com>
2026-04-21 22:10:12 +0200

    200 1

--- 21-04-2026_28-04-2026.py
+++ 21-04-2026_28-04-2026.py
@@ -25,3 +25,21 @@ class Solution35:
 class Solution58:
     def lengthOfLastWord(self, s: str):
         return len(s.strip().split(' ')[-1])
+
+
+# 21.04.2026
+# 200 medium
+class Solution200:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        number_of_islands = 0
+        for spisok_index, spisok in enumerate(grid):
+            for yacheika_index, yacheika in enumerate(spisok):
+                if yacheika == '1':
+                    if spisok[yacheika_index+1] == '1' or grid[spisok_index+1][yacheika_index] == '1':
+                        continue
+
+        return number_of_islands
+
+
+solution = Solution200()
+print(solution.numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))
